c3a5243445949732871be12dd72643e4787018c3
Android Gifts <info@androidgifts.com>
2016-02-03 17:16:06 +0200

    Comments CRUD is done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,42 @@ class CommentsController < ApplicationController
   def new
   end
 
+  def create
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.create(params[:comment].permit(:content))
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to message_path(@message)
+    else
+      render('new')
+    end
+  end
+
   def edit
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.find(params[:id])
+
+    if @comment.update(params[:comment].permit(:content))
+      redirect_to message_path(@message)
+    else
+      render 'edit'
+    end
   end
 
   def delete
+  end
+
+  def destroy
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.find(params[:id])
+
+    @comment.destroy
+    redirect_to message_path(@message)
   end
 end
